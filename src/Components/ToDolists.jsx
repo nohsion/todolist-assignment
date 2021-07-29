19a75c1692226d215cad3ef6efbae2b3f8fae3e2
Sion Noh <shtldhs123@ajou.ac.kr>
2021-07-29 17:26:14 +0900

    update toggle 추가 (card에 반영되지 않음)

--- src/Components/ToDolists.jsx
+++ src/Components/ToDolists.jsx
@@ -1,41 +1,97 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import PropTypes from "prop-types"
-import { Button, Card, CardTitle, CardText, Container } from "reactstrap"
+import {Button, Card, CardTitle, CardText, Container, Input} from "reactstrap"
 
-const ToDOLists = ({ data, deleteToDo, upDateToDo }) => {
-  const handleOnClick = id => e => {
-    deleteToDo(id)
-  }
+const ToDOLists = ({data, deleteToDo, upDateToDo}) => {
+    const handleOnClick = id => e => {
+        deleteToDo(id)
+    }
+    const [toggle, setToggle] = useState(false)
+    const editOnClick = id => e => {
+        setToggle(true)
+        upDateToDo(id, data)
+    }
 
-  return (
-    <Container>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          gap: "10px"
-        }}
-      >
-        {data.map((v, i) => (
-          <Card body inverse color="primary" key={v.id}>
-            <CardTitle tag="h5">{v.title}</CardTitle>
-            <CardText>{v.description}</CardText>
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
-              <Button color="secondary" onClick={handleOnClick(v.id)}>
-                Delete
-              </Button>
-              <Button color="secondary">Update</Button>
+    /* update */
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    useEffect(() => {
+        window.addEventListener("keypress", handleKeyPress)
+        return () => {
+            window.removeEventListener("keypress", handleKeyPress)
+        }
+    }, [title, description])
+
+    const handleKeyPress = event => {
+        console.log(event)
+        if (event.code === "Enter") {
+            if (title && description) {
+                upDateToDo(Date.now(),
+                    {
+                    id: Date.now(),
+                    title: title,
+                    description: description
+                }, setToggle(false))
+            }
+        }
+    }
+    const onChangeTitle = e => {
+        setTitle(e.target.value)
+    }
+
+    const onChangeDescription = e => {
+        setDescription(e.target.value)
+    }
+    /* ****** */
+
+    return (
+        <Container>
+            <div
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: "1fr 1fr 1fr",
+                    gap: "10px"
+                }}
+            >
+                {data.map((v, i) => (
+                    <Card body inverse color="primary" key={v.id}>
+                        <CardTitle tag="h5">{v.title}</CardTitle>
+                        <CardText>{v.description}</CardText>
+                        <div style={{display: "flex", justifyContent: "space-around"}}>
+                            <Button color="secondary" onClick={handleOnClick(v.id)}>
+                                Delete
+                            </Button>
+                            <Button color="secondary" onClick={editOnClick(v.id)}>
+                                Update
+                            </Button>
+                        </div>
+                    </Card>
+                ))}
             </div>
-          </Card>
-        ))}
-      </div>
-    </Container>
-  )
+
+            {toggle ? (<div>
+                <Input
+                    placeholder="Title"
+                    value={title}
+                    onChange={onChangeTitle}
+                    valid={title.length > 0 ? true : false}
+                />
+                <Input
+                    placeholder="Description"
+                    value={description}
+                    onChange={onChangeDescription}
+                    valid={description.length > 0 ? true : false}
+                />
+            </div>) : (<></>) }
+
+        </Container>
+    )
 }
 export default ToDOLists
 
 ToDOLists.propTypes = {
-  data: PropTypes.array.isRequired,
-  deleteToDo: PropTypes.func.isRequired,
-  upDateToDo: PropTypes.func.isRequired
+    data: PropTypes.array.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    upDateToDo: PropTypes.func.isRequired
 }
--- src/Router/App.js
+++ src/Router/App.js
@@ -1,44 +1,61 @@
-import React, { useState } from "react"
+import React, {useState} from "react"
 //
 import Header from "../Components/Header"
 import ToDolists from "../Components/ToDolists"
 
 function App() {
-  const [toDos, setToDos] = useState([
-    {
-      id: 213213421,
-      title: "코딩하기",
-      description: "나는 오늘 리액트에 대해 배웠다. 정말 재미가 있었다."
-    },
-    {
-      id: 12421424,
-      title: "밥먹기",
-      description: "밥을 먹었다 정말 맛있다."
-    },
-    {
-      id: 2143261,
-      title: "운동하기",
-      description: "나는 복싱한다. 고로 생각한다."
+    const [toDos, setToDos] = useState([
+        {
+            id: 213213421,
+            title: "코딩하기",
+            description: "나는 오늘 리액트에 대해 배웠다. 정말 재미가 있었다."
+        },
+        {
+            id: 12421424,
+            title: "밥먹기",
+            description: "밥을 먹었다 정말 맛있다."
+        },
+        {
+            id: 2143261,
+            title: "운동하기",
+            description: "나는 복싱한다. 고로 생각한다."
+        }
+    ])
+
+    const addToDo = data => {
+        console.log(data)
+        setToDos([...toDos, data])
     }
-  ])
 
-  const addToDo = data => {
-    setToDos([...toDos, data])
-  }
+    const deleteToDo = id => {
+        const deleteToDos = toDos.filter(item => item.id !== id)
+        setToDos(deleteToDos)
+    }
 
-  const deleteToDo = id => {
-    const deleteToDos = toDos.filter(item => item.id !== id)
-    setToDos(deleteToDos)
-  }
+    const upDateToDo = (id, data) => {
+        const new_toDos = [...toDos]
 
-  const upDateToDo = (id, data) => {}
+        if (data.id !== undefined) {
+            console.log(data.id, data.title, data.description)
 
-  return (
-    <>
-      <Header addToDo={addToDo} />
-      <ToDolists data={toDos} deleteToDo={deleteToDo} upDateToDo={upDateToDo} />
-    </>
-  )
+            for (let i = 0; i < new_toDos.length; i++) {
+                if (new_toDos[i].id === id) {
+                    new_toDos[i].id = data.id
+                    new_toDos[i].title = data.title
+                    new_toDos[i].description = data.description
+                    // console.log(new_toDos[i].id, new_toDos[i].title, new_toDos[i].description)
+                }
+            }
+        }
+        setToDos(new_toDos)
+    }
+
+    return (
+        <>
+            <Header addToDo={addToDo}/>
+            <ToDolists data={toDos} deleteToDo={deleteToDo} upDateToDo={upDateToDo}/>
+        </>
+    )
 }
 
 export default App
